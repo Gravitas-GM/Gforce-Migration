d7405981e13acd8867efead6f034651985577c88
Tyler Lartonoix <larttyler@live.com>
2015-06-29 14:52:17 -0400

    WIP

--- bootstrap.php
+++ bootstrap.php
@@ -1,23 +1,26 @@
 <?php
+	ini_set('auto_detect_line_endings', true);
+
 	require __DIR__ . '/vendor/autoload.php';
 
 	define('SYSTEM_CONFIG_FILE', __DIR__ . '/config.yml');
 	define('SYSTEM_WSDL_FILE', __DIR__ . '/sf.partner.wsdl.xml');
+	define('SYSTEM_CSV_FILE', __DIR__ . '/list.csv');
 
 	define('LOGGING_GLOBAL_CHANNEL', 'bootstrap');
 	define('LOGGING_LOG_FILE', __DIR__ . '/logs/log.log');
-	define('LOGGING_FORMAT', "[%datetime%] [%extra.uid%] %channel%.%level_name%: %message% %context% %extra%\n");
-	define('LOGGING_GLOBAL_LEVEL', Monolog\Logger::WARNING);
+	define('LOGGING_FORMAT', "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n");
+	define('LOGGING_GLOBAL_LEVEL', Monolog\Logger::DEBUG);
 
 	Monolog\Registry::addLogger(makeLogger(LOGGING_GLOBAL_CHANNEL, LOGGING_GLOBAL_LEVEL));
 
-	set_error_handler(function($number, $text) {
+	set_error_handler(function($number, $text, $file, $line) {
 		if (!(error_reporting() & $number))
 			return;
 
 		getActiveLogger()->critical($text);
 
-		printf('<pre>%s</pre>', $text);
+		printf("%s in %s on line %d\n", $text, $file, $line);
 
 		die();
 	});
@@ -54,9 +57,14 @@
 
 	function getActiveLogger() {
 		if (defined('LOGGING_CHANNEL')) {
-			if (!Monolog\Registry::hasLogger(LOGGING_CHANNEL))
-				Monolog\Registry::addLogger(makeLogger(LOGGING_CHANNEL, defined('LOGGING_LEVEL') ? LOGGING_LEVEL :
-					LOGGING_GLOBAL_LEVEL));
+			if (!Monolog\Registry::hasLogger(LOGGING_CHANNEL)) {
+				$level = defined('LOGGING_LEVEL') ? LOGGING_LEVEL : LOGGING_GLOBAL_LEVEL;
+
+				if (is_string($level))
+					$level = (new ReflectionClass('Monolog\\Logger'))->getConstant($level);
+
+				Monolog\Registry::addLogger(makeLogger(LOGGING_CHANNEL, $level));
+			}
 
 			return Monolog\Registry::getInstance(LOGGING_GLOBAL_CHANNEL);
 		}
--- pass1.php
+++ pass1.php
@@ -1,10 +1,106 @@
 <?php
+	define('LOGGING_CHANNEL', 'pass1');
+	define('LOGGING_LEVEL', 'DEBUG');
+	define('DRY_RUN', true);
+
 	require __DIR__ . '/bootstrap.php';
 
-	define('LOGGING_CHANNEL', 'pass1');
-	define('LOGGING_LEVEL', LOGGING_GLOBAL_LEVEL);
+	use \Exception;
 
+	use \SObject;
+
+	use DaybreakStudios\Common\IO\CsvFileReader;
 	use DaybreakStudios\Salesforce\Client;
 
+	$logger = getActiveLogger();
 	$client = new Client($config['sf.username'], $config['sf.token'], SYSTEM_WSDL_FILE);
+
+	if (!file_exists(SYSTEM_CSV_FILE))
+		throw new Exception('Could not locate ' . SYSTEM_CSV_FILE);
+
+	$f = fopen(SYSTEM_CSV_FILE, 'r');
+
+	if ($f === false)
+		throw new Exception('Could not open ' . SYSTEM_CSV_FILE . ' for reading');
+
+	// Dealership, First Name, Last Name, Position, Email, Phone, Street, County, City, State, Zip Code
+	$reader = new CsvFileReader($f);
+	$reader
+		->addField('dealership')
+		->addField('firstName')
+		->addField('lastName')
+		->addField('position')
+		->addField('email')
+		->addField('phone', 'cleanPhone')
+		->addField('street')
+		->addField('county')
+		->addField('city')
+		->addField('state')
+		->addField('zip');
+
+	while (!$reader->eof()) {
+		$row = $reader->read();
+
+		$logger->debug('-- Read CSV row', [ $row ]);
+
+		$account = $client->query('select Id from Account where Name = :dealership limit 1', [
+			':dealership' => $row->dealership,
+		]);
+
+		if ($account->size === 0) {
+			$logger->info('No match for dealership named ' . $row->dealership . '; saving for pass 2');
+
+			continue;
+		}
+
+		$account = $account->records[0];
+
+		$logger->debug('Found Account with Id ' . $account->Id, [ $account ]);
+
+		$result = $client->query('select Id, Phone from Contact where AccountId = :id', [
+			':id' => $account->Id,
+		]);
+
+		foreach ($result->records as $record) {
+			$phone = cleanPhone($record->fields->Phone);
+
+			if ($row->phone === $phone) {
+				$logger->debug('Found matching phones; ' . $record->Id . ' has phone number ' . $row->phone);
+
+				continue 2;
+			}
+		}
+
+		$sob = new SObject();
+		$sob->type = 'Contact';
+		$sob->fields = [
+			'AccountId' => $account->Id,
+			'FirstName' => $row->firstName,
+			'LastName' => $row->lastName,
+			'Job_Title__c' => $row->position,
+			'Street' => $row->street,
+			'City' => $row->city,
+			'State' => $row->state,
+			'PostalCode' => $row->zip,
+		];
+
+		$logger->info('Creating new Contact on Account (' . $account->Id . ')', [ $sob ]);
+
+		if (!DRY_RUN) {
+			$result = $client->create($sob);
+
+			$logger->debug('Created new Contact', [ $result ]);
+		} else
+			printf("Adding new contact with phone %s to %s\n", $row->phone, $account->Id);
+	}
+
+	function cleanPhone($value) {
+		$v = '';
+
+		for ($i = 0, $ii = strlen($value); $i < $ii; $i++)
+			if (is_numeric($value[$i]))
+				$v .= $value[$i];
+
+		return $v;
+	}
 ?>
